3a06fff15744abbffd9b8551a0384873002da53e
aydnorcn <orcnaydn3535@gmail.com>
2024-10-26 19:30:24 +0300

    fix: CartRequest quantity validation issue

--- src/main/java/com/aydnorcn/food_order/dto/cart/CartRequestDto.java
+++ src/main/java/com/aydnorcn/food_order/dto/cart/CartRequestDto.java
@@ -1,10 +1,16 @@
 package com.aydnorcn.food_order.dto.cart;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CartRequestDto {
 
+    @NotNull(message = "Food id is required")
     private Long foodId;
+
+    @NotNull(message = "Quantity is required")
+    @DecimalMin(value = "1", message = "Quantity must be greater than 0")
     private Integer quantity;
 }
